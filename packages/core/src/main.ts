1bbda093112d029573cb1e1dd5b562364550e74a
fredrik <fredrik.lowenhamn@gmail.com>
2023-04-21 09:27:14 +0200

    Wip

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,16 +1,16 @@
-import {createReactWrapper} from 'mfdev/react'
-import {registerLazyComponents} from 'mfdev/lazy'
+import { createReactWrapper } from 'mfdev/react'
+import { registerLazyComponents } from 'mfdev/lazy'
 import 'mfdev/polyfill'
-import {createBrowserHistory} from 'history'
+import { createBrowserHistory } from 'history'
 
 import App from './App.js'
-import {createRoot} from "react-dom/client";
-import {createElement} from "react";
+import { createRoot } from 'react-dom/client'
+import { createElement } from 'react'
 
 // createReactWrapper({tag: 'ex-core', component: App})
 window.HistoryLibrary = createBrowserHistory()
 
-const root = createRoot(document.body);
-root.render(createElement(App));
+const root = createRoot(document.body)
+root.render(createElement(App))
 
-// registerLazyComponents(await fetch('./public/lazy.json').then(r => r.json()), false)
+// registerLazyComponents(await fetch('./public/assets.json').then(r => r.json()), false)
