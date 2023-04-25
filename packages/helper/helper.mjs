1e4fda4af9d8bb61d9bfde73e881ca43e80c07a2
fredrik <fredrik.lowenhamn@gmail.com>
2023-04-25 10:23:11 +0200

    Wip

---
+++ packages/helper/helper.mjs
@@ -0,0 +1,29 @@
+import express from "express";
+import util from "util";
+import { exec } from "child_process";
+
+const asyncExec = util.promisify(exec);
+
+const listPorts = async () => {
+  const result = await asyncExec("lsof -i -P -n | grep LISTEN");
+  const output = result.stdout;
+  const ports = output.split("\n")
+    .map(l => l.split(" ")
+    .filter(d => d))
+    .filter(l=>l[0]==="node")
+    .map(l=>l[8].split(":")[1])
+  return (await Promise.all(ports.map(
+    port => fetch(`http://localhost:${port}/__mfdev`)
+      .then(r => r.json().then(j => ({ ...j, port })))
+      .catch(() => undefined)))).filter(r => !!r)
+};
+listPorts();
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  res.setHeader("content-type", "application/json");
+  res.send(JSON.stringify(await listPorts(), null ,2));
+});
+
+app.listen(1234);
