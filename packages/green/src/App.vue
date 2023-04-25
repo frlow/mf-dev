e85a7212d1652567cd4b32dd2db051037bdff936
fredrik <fredrik.lowenhamn@gmail.com>
2023-04-25 11:31:12 +0200

    Wip

--- packages/green/src/App.vue
+++ packages/green/src/App.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
+const increment = () => {
+  count.value++
+}
 </script>
 
 <template>
-  <h1>GREEN</h1>
+  <div>
+    <h1>GREEN</h1>
+    <button is="ex-button" @click="increment">Clicked: {{ count }}</button>
+  </div>
 </template>
 
 <style scoped>
-h1{
-    color: #41B883;
+h1 {
+  color: #41b883;
 }
 </style>
