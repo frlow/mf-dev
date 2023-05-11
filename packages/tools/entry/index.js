cc2f17370eb4389152967137431a218a13014c4c
fredrik <fredrik.lowenhamn@gmail.com>
2023-05-11 10:09:17 +0200

    Wip

--- packages/tools/entry/index.js
+++ packages/tools/entry/index.js
@@ -12,22 +12,24 @@ export const entry = (options) => ({
       return path.join(getDir(options), '___entry.js')
   },
   config(userConfig) {
+    if (!userConfig.build) userConfig.build = {}
+    if (!userConfig.build.rollupOptions) userConfig.build.rollupOptions = {}
+    if (!userConfig.build.rollupOptions.output)
+      userConfig.build.rollupOptions.output = {}
+    if (!userConfig.build.rollupOptions.output.assetFileNames)
+      userConfig.build.rollupOptions.output.assetFileNames = `${options.name}-[name]-[hash][extname]`
+    if (!userConfig.build.rollupOptions.output.chunkFileNames)
+      userConfig.build.rollupOptions.output.chunkFileNames = `${options.name}-[name]-[hash].js`
+    if (!userConfig.build.rollupOptions.output.entryFileNames)
+      userConfig.build.rollupOptions.output.entryFileNames =
+        options.name + (options.hashEntry ? '-[hash]' : '') + '.js'
     userConfig.base = './'
+    userConfig.build.rollupOptions.input = ['___entry.js']
   },
   async load(id) {
     if (id.endsWith('___entry.js'))
       return `import('./${
         options.entry ? path.parse(options.entry).name : 'main'
       }')`
-  },
-  async buildStart(buildOptions) {
-    buildOptions.input = ['___entry.js']
-  },
-  outputOptions(outputOptions) {
-    outputOptions.assetFileNames = `${options.name}-[name]-[hash][extname]`
-    outputOptions.chunkFileNames = `${options.name}-[name]-[hash].js`
-    outputOptions.entryFileNames =
-      options.name + (options.hashEntry ? '-[hash]' : '') + '.js'
-    return outputOptions
   },
 })
