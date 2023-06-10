0047e986203874d5ffa1167d371ce6f08c39a103
fredrik <fredrik.lowenhamn@gmail.com>
2023-06-10 13:54:25 +0200

    Wip

--- packages/example/apps/blue/vite.config.ts
+++ packages/example/apps/blue/vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-import * as path from 'path'
-
-const name = path.parse(__dirname).name
 
 // https://vitejs.dev/config/
 export default defineConfig({
--- packages/example/apps/core/vite.config.ts
+++ packages/example/apps/core/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'path'
 import { reactPreamble } from '@mf-dev/react-preamble'
-
-const name = path.parse(__dirname).name
 
 // https://vitejs.dev/config/
 export default defineConfig({
