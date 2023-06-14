c0ba70e3432a561d51a4e6f6af7df13fa70631d7
fredrik <fredrik.lowenhamn@gmail.com>
2023-06-14 08:06:57 +0200

    Wip

--- upgrade.sh
+++ upgrade.sh
@@ -1,10 +1,11 @@
-reference="axios"
+reference="@mf-dev/wrapper-common"
 current=$(pnpm pkg get version | sed -r 's/\"//g')
 remote=$(pnpm show $reference --version)
 if [ "$current" != "$remote" ];
 then
   echo "new version, publishing $current"
   pnpm -r --filter="@mf-dev/*" exec pnpm version --no-git-tag-version $current
+  pnpm -r publish --dry-run --no-git-checks
 else
   echo "same version"
 fi
