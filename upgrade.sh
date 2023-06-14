f7df65c596ff1c32bb604ade6f352d58014c060b
fredrik <fredrik.lowenhamn@gmail.com>
2023-06-14 09:45:14 +0200

    Wip

--- upgrade.sh
+++ upgrade.sh
@@ -3,9 +3,14 @@ current=$(pnpm pkg get version | sed -r 's/\"//g')
 remote=$(pnpm show $reference --version)
 if [ "$current" != "$remote" ];
 then
+  npm config -L project set '//registry.npmjs.org/:_authToken' $1
   echo "new version, publishing $current"
   pnpm -r --filter="@mf-dev/*" exec pnpm version --no-git-tag-version $current
-  pnpm -r publish --dry-run --no-git-checks
+  pnpm i
+  pnpm -r publish --no-git-checks --access public
+  pnpm -r --filter="@mf-dev/*" exec pnpm version --no-git-tag-version 0.0.0
+  pnpm i
+  rm .npmrc
 else
   echo "same version"
 fi
