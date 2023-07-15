909af531fc49f7b742dfeebd5cfae42b18e463f1
fredrik <fredrik.lowenhamn@gmail.com>
2023-07-15 23:02:50 +0200

    Wip

--- packages/wrappers/common/index.js
+++ packages/wrappers/common/index.js
@@ -1,12 +1,9 @@
-/** @type {(wrapperClass: any, attributes: string[])=>void} **/
 export const applyProps = (wrapperClass, attributes) => {
-  attributes.forEach((attribute) => {
-    const setter = {
+  attributes.forEach((attribute) =>
+    Object.defineProperty(wrapperClass.prototype, attribute, {
       set: function (value) {
-        // @ts-ignore
         this.updateProp(attribute, value)
       },
-    }
-    Object.defineProperty(wrapperClass.prototype, attribute, setter)
-  })
+    })
+  )
 }
