86338bc9084ad172ca5542a4511b382ccae353d3
fredrik <fredrik.lowenhamn@gmail.com>
2023-07-15 23:25:41 +0200

    Wip

--- packages/wrappers/solid/index.d.ts
+++ packages/wrappers/solid/index.d.ts
@@ -1,4 +1,4 @@
-import { WrapperOptions } from '@mf-dev/wrapper-common/index'
+import { WrapperOptions } from '@mf-dev/wrapper-common'
 
 export * from '@mf-dev/wrapper-common'
 export const createSolidWrapper: (options: WrapperOptions) => void
