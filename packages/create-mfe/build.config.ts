eeafbd1e823da18613a91163641f8aa970e5a52f
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2023-10-17 10:36:50 +0200

    Wip

--- packages/create-mfe/build.config.ts
+++ packages/create-mfe/build.config.ts
@@ -1,9 +1,4 @@
-import path from 'node:path'
-import url from 'node:url'
 import { defineBuildConfig } from 'unbuild'
-import licensePlugin from '../vite/rollupLicensePlugin'
-
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 export default defineBuildConfig({
   entries: ['src/index'],
@@ -15,20 +10,14 @@ export default defineBuildConfig({
       minify: true,
     },
   },
+  failOnWarn: false,
   alias: {
     // we can always use non-transpiled code since we support node 18+
     prompts: 'prompts/lib/index.js',
   },
   hooks: {
     'rollup:options'(ctx, options) {
-      options.plugins = [
-        options.plugins,
-        licensePlugin(
-          path.resolve(__dirname, './LICENSE'),
-          'create-vite license',
-          'create-vite',
-        ),
-      ]
+      options.plugins = [options.plugins]
     },
   },
 })
