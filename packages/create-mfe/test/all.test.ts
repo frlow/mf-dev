aa1a1871ff4ca04d11934153f43e73efbc5b8fff
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2023-10-27 13:35:24 +0200

    Wip

--- packages/create-mfe/test/all.test.ts
+++ packages/create-mfe/test/all.test.ts
@@ -3,40 +3,40 @@ import { baseTest } from './common/base'
 
 test('react dev', async () => {
   await baseTest('react', 'dev')
-})
+}, 15000)
 
 test('react preview', async () => {
   await baseTest('react', 'preview')
-})
+}, 15000)
 
 test('vue dev', async () => {
   await baseTest('vue', 'dev')
-})
+}, 15000)
 
 test('vue preview', async () => {
   await baseTest('vue', 'preview')
-})
+}, 15000)
 
 test('svelte dev', async () => {
   await baseTest('svelte', 'dev')
-})
+}, 15000)
 
 test('svelte preview', async () => {
   await baseTest('svelte', 'preview')
-})
+}, 15000)
 
 test('solid dev', async () => {
   await baseTest('solid', 'dev')
-}, 60000)
+}, 15000)
 
 test('solid preview', async () => {
   await baseTest('solid', 'preview')
-})
+}, 15000)
 
 test('qwik dev', async () => {
   await baseTest('qwik', 'dev')
-})
+}, 15000)
 
 test('qwik preview', async () => {
   await baseTest('qwik', 'preview')
-})
+}, 15000)
--- packages/create-mfe/test/setup.js
+++ packages/create-mfe/test/setup.js
@@ -1,5 +1,5 @@
-import { promisify } from 'util'
-import { exec } from 'child_process'
+import {promisify} from 'util'
+import {exec} from 'child_process'
 import * as fs from 'fs'
 import path from 'node:path'
 
@@ -9,21 +9,21 @@ await execAsync('rm -rf mfe-test-*')
 await execAsync('pnpm i')
 await execAsync('pnpm templates')
 const dirs = fs
-  .readdirSync('templates')
-  .filter((d) => fs.lstatSync(path.join('templates', d)).isDirectory())
+    .readdirSync('templates')
+    .filter((d) => fs.lstatSync(path.join('templates', d)).isDirectory())
 await execAsync(
-  'pnpm --filter "@mf-dev/wrapper*" exec npm pack --pack-destination ../create-mfe/templates'
+    'pnpm --filter "@mf-dev/wrapper*" exec npm pack --pack-destination ../create-mfe/templates'
 )
-for (const dir of dirs) {
-  const name = dir.replace('template-', '')
-  const projectDir = `mfe-test-${dir}`
-  await execAsync(`pnpm dev ${projectDir} --template ${name}`)
-  const pkgPath = `${projectDir}/package.json`
-  const pkg = fs.readFileSync(pkgPath, 'utf8')
-  const lines = pkg.split('\n').filter((l) => !l.includes('latest'))
-  const fixedPkg = lines.join('\n')
-  fs.writeFileSync(pkgPath, fixedPkg, 'utf8')
-  await execAsync(
-    `cd ${projectDir} && npm i && npm i --save-dev ../templates/*${name}*.tgz && npm run build`
-  )
-}
+await Promise.all(dirs.map(async dir => {
+    const name = dir.replace('template-', '')
+    const projectDir = `mfe-test-${dir}`
+    await execAsync(`pnpm dev ${projectDir} --template ${name}`)
+    const pkgPath = `${projectDir}/package.json`
+    const pkg = fs.readFileSync(pkgPath, 'utf8')
+    const lines = pkg.split('\n').filter((l) => !l.includes('latest'))
+    const fixedPkg = lines.join('\n')
+    fs.writeFileSync(pkgPath, fixedPkg, 'utf8')
+    await execAsync(
+        `cd ${projectDir} && npm i && npm i --save-dev ../templates/*${name}*.tgz && npm run build`
+    )
+}))
