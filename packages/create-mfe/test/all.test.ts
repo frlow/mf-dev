d42949cc31f80304c3df1fa0ba001fd7e91382d8
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2023-10-27 14:24:05 +0200

    Wip

--- packages/create-mfe/test/all.test.ts
+++ packages/create-mfe/test/all.test.ts
@@ -1,42 +1,44 @@
-import { test } from 'vitest'
-import { baseTest } from './common/base'
+import {test} from 'vitest'
+import {baseTest} from './common/base'
+
+const testDev = process.env.MODE === "dev"
 
 test('react dev', async () => {
-  await baseTest('react', 'dev')
+  await baseTest('react', 'dev', testDev)
 }, 15000)
 
 test('react preview', async () => {
-  await baseTest('react', 'preview')
+  await baseTest('react', 'preview', testDev)
 }, 15000)
 
 test('vue dev', async () => {
-  await baseTest('vue', 'dev')
+  await baseTest('vue', 'dev', testDev)
 }, 15000)
 
 test('vue preview', async () => {
-  await baseTest('vue', 'preview')
+  await baseTest('vue', 'preview', testDev)
 }, 15000)
 
 test('svelte dev', async () => {
-  await baseTest('svelte', 'dev')
+  await baseTest('svelte', 'dev', testDev)
 }, 15000)
 
 test('svelte preview', async () => {
-  await baseTest('svelte', 'preview')
+  await baseTest('svelte', 'preview', testDev)
 }, 15000)
 
 test('solid dev', async () => {
-  await baseTest('solid', 'dev')
+  await baseTest('solid', 'dev', testDev)
 }, 15000)
 
 test('solid preview', async () => {
-  await baseTest('solid', 'preview')
+  await baseTest('solid', 'preview', testDev)
 }, 15000)
 
 test('qwik dev', async () => {
-  await baseTest('qwik', 'dev')
+  await baseTest('qwik', 'dev', testDev)
 }, 15000)
 
 test('qwik preview', async () => {
-  await baseTest('qwik', 'preview')
+  await baseTest('qwik', 'preview', testDev)
 }, 15000)
--- packages/create-mfe/test/common/base.ts
+++ packages/create-mfe/test/common/base.ts
@@ -1,10 +1,12 @@
-import { startVite } from './vite'
-import { parseReadme } from './readme'
-import { expect } from 'vitest'
-import { parseWeb } from './web'
+import {startVite} from './vite'
+import {parseReadme} from './readme'
+import {expect} from 'vitest'
+import {parseWeb} from './web'
 
-export const baseTest = async (framework: string, mode: 'dev' | 'preview') => {
-  const basePath = `mfe-test-template-${framework}`
+export const baseTest = async (framework: string, mode: 'dev' | 'preview', testDev: boolean) => {
+  const basePath = testDev
+      ? `../../templates/template-${framework}`
+      : `mfe-test-template-${framework}`
   const viteServer = await startVite(basePath, mode)
   const readme = parseReadme(basePath)
   expect(readme[mode]).not.toBeUndefined()
