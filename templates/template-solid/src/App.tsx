3826d731ea0ab61bf3d2ed81a6d9efaa626e2c26
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2023-11-21 11:56:13 +0100

    Wip

--- templates/template-solid/src/App.tsx
+++ templates/template-solid/src/App.tsx
@@ -1,18 +1,24 @@
 import 'solid-js'
 import style from './App.module.scss'
+import {from} from "solid-js";
+
+const {mySignal, fizzBuzz} = (window as any).store
 
 const App = (props: {
-  mycount: ()=>number
+  mycount: () => number
   dispatch: (name: string, detail?: any) => void
 }) => {
-  const clicks = ()=>`Clicks: ${props.mycount()}`
+  const clicks = () => `Clicks: ${props.mycount()}`
   return (
-    <div class={style.root}>
-      <h1>solid</h1>
-      <solid-button text={clicks()} onmyclick={() => {
-        props.dispatch('myevent', 'solid')
-      }}></solid-button>
-    </div>
+      <div class={style.root}>
+        <h1>solid</h1>
+        <solid-button text={clicks()} onmyclick={() => {
+          props.dispatch('myevent', 'solid')
+        }}></solid-button>
+        <solid-button text={`${from(mySignal)()} = ${from(fizzBuzz)()}`} onmyclick={() => {
+          mySignal.value++
+        }}></solid-button>
+      </div>
   )
 }
 export default App
