1d722015b1b2b1a7ef49fbdbc859159240deaab2
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2024-01-25 01:11:01 +0100

    Fixed Bug

--- packages/wrapper-qwik/index.d.ts
+++
@@ -1,8 +0,0 @@
-export type WrapperOptions = {
-  component: () => Promise<any>
-  attributes?: string[]
-  tag: string
-  shadowRoot?: 'open' | 'closed'
-}
-
-export const createQwikWrapper: (options: WrapperOptions) => void
